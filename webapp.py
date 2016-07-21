3a35546d10a0333aced3f16c45c11c006ddaf294
Your Name <musa16>
2016-07-21 14:27:23 +0300

    -mousa i fixed the about us responsive

--- webapp.py
+++ webapp.py
@@ -19,6 +19,7 @@ def home():
 
 @app.route("/signup", methods = ['GET', 'POST'])
 def signup():
+
 	if request.method == 'GET':
 		return render_template('signup.html')
 	else:
@@ -28,11 +29,18 @@ def signup():
 		new_username = request.form['username']
 		new_password = request.form['password']
 		confirm_password = request.form['confirm_password']
+		print ("beginig of the test")
+		print (confirm_password)
+		print (new_password)
+		print ("the end of the test")
 		if confirm_password == new_password:
 			person = User(first_name = new_firstname, last_name = new_lastname, email = new_email, username = new_username, password = new_password)
 			session.add(person)
 			session.commit()
 			return redirect(url_for('view_profile', user_id = person.id))
+		else:
+			error = 'The passwords do not match'
+			return error
 
 
 @app.route("/login", methods = ['GET', 'POST'])
@@ -47,7 +55,9 @@ def login():
 			error = 'User does not exist '
 			return render_template('login.html', error = error)
 		else:
-			return redirect(url_for('view_profile', user_id = person.id))
+			if person.password == request.form['password']:
+				return redirect(url_for('view_profile', user_id = person.id))
+			return render_template('login.html', error = error)
 
 
 @app.route("/profile/<int:user_id>")
@@ -83,7 +93,26 @@ def contactus():
 
 
 
+@app.route('/edit/<int:user_id>', methods=['GET', 'POST'])
+def edit_profile(user_id):
+    person = session.query(User).filter_by(id=user_id).first()
+    print([person])
+    if request.method == 'GET':
+        return render_template('edit_profile.html', person = person)
+    else:
+        new_username = request.form['username']
+        new_password = request.form['password']
+        new_conpass = request.form['conpass']
+        new_email = request.form['email']
 
+
+        person.username = new_username
+        person.password = new_password
+        person.email = new_email
+
+        session.commit()
+
+        return redirect(url_for('view_profile', user_id = person.id))
 
 
 @app.route("/aboutus")
